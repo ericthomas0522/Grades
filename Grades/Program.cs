b8ec4ad2e07763a360365310d7e91cefc28897e9
Eric Thomas <eric.therock@gmail.com>
2018-07-28 14:32:41 -0500

    End of the gradebook exercise

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -13,8 +13,16 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(95);
             book.AddGrade(89.5f);
+            book.AddGrade(77);
 
             GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.AverageGrade);
+
+            //keeps the program open so I can see the results.
+            Console.ReadLine();
         }
     }
 }
